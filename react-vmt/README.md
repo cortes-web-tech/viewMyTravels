f5eb8d5e0f94160be6d713efa081e05bbcac1897
cortes-web-tech <cortes.web.tech@gmail.com>
2023-07-03 20:00:34 -0400

    v0.0.3

--- react-vmt/README.md
+++ react-vmt/README.md
@@ -5,3 +5,4 @@ _Ported app to React because of issues hosting/rendering Svelte app_
 ## v0.0.3
 
 _Displaying map on home page_
+_Code cleanup, marker displaying on map_
--- react-vmt/src/components/MapWindow.js
+++ react-vmt/src/components/MapWindow.js
@@ -1,24 +1,30 @@
 import React from "react";
 import "../App.css";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useState, useEffect } from "react";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 function MapWindow() {
   console.log(process.env.REACT_APP_GCP_MAPS_API_KEY);
-  const longitude = -105.20575;
-  const latitude = 39.66528666666667;
-  const { isLoaded } = useLoadScript({
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_GCP_MAPS_API_KEY,
   });
   return <div>{isLoaded ? <Map /> : "loading.."}</div>;
 }
 
 function Map() {
+  const [longitude, setLongitude] = useState(-105.20575);
+  const [latitude, setlatitude] = useState(39.66528666666667);
+
   return (
     <GoogleMap
       zoom={18}
-      center={{ lat: 39.66528666666667, lng: -105.20575 }}
+      center={{ lat: latitude, lng: longitude }}
       mapContainerClassName="mapWindow"
       mapTypeId="satellite"
-    ></GoogleMap>
+    >
+      <Marker position={{ lat: latitude, lng: longitude }} />
+    </GoogleMap>
   );
 }
 
